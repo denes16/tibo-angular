f08dba36b3ffc3634fa5503a5ed18e96db50fa3c
Denes <74267424+denes16@users.noreply.github.com>
2020-11-25 20:08:53 -0600

    Ruta inventario creada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./routing.module"; // CLI imports AppRoutingModule
 import { AppComponent } from "./app.component";
+import { InventarioComponent } from "./components/inventario/inventario.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InventarioComponent],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule // CLI adds AppRoutingModule to the AppModule's imports array
   ],
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router"; // CLI imports router
 import { HomeComponent } from "./components/home/home.component";
+import { InventarioComponent } from "./components/inventario/inventario.component";
 const routes: Routes = [
-   {path: "home", component: HomeComponent },
-  {path: "**", pathMatch: "full", redirectTo:'home'  }
-  ]; 
+  { path: "home", component: HomeComponent },
+  { path: "inventario", component: InventarioComponent },
+  { path: "**", pathMatch: "full", redirectTo: "/inventario" }
+];
 
 // configures NgModule imports and exports
 @NgModule({
